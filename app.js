ef2ba3cdc5888868254372970576ea5471969839
Indio Ferrel <mferrel1911@gmail.com>
2022-04-29 17:05:57 -0700

    Completed displayCatchphrases() and corrected errors in dropdown functions.

--- app.js
+++ app.js
@@ -20,57 +20,59 @@ let catchphrases = [];
 
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
-    let value = headDropdown.value;
+    let headValue = headDropdown.value;
     // increment the head change count state
     headCount++;
     // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
-    headEl.style.backgroundImage = `url("./assets/${value}-head.png")`;
+    headEl.style.backgroundImage = `url("./assets/${headValue}-head.png")`;
     // update the stats to show the new count (call displayStats() to do this work)
     displayStats();
 });
 
 middleDropdown.addEventListener('change', () => {
     // get the value of the middle dropdown
-    let value = middleDropdown.value;
+    let middleValue = middleDropdown.value;
     // increment the middle change count state
     middleCount++;
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
-    middleEl.style.backgroundImage = `url("./assets/${value}-middle.png")`;
+    middleEl.style.backgroundImage = `url("./assets/${middleValue}-middle.png")`;
     // update the stats to show the new count (call displayStats() to do this work)
     displayStats();
 });
 
 bottomDropdown.addEventListener('change', () => {
     // get the value of the bottom dropdown
-    let value = bottomDropdown.value;
+    let botValue = bottomDropdown.value;
+    console.log(botValue);
     // increment the bottom change count state
     bottomCount++;
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-    bottomEl.style.backgroundImage = `url("./assets/${value}-bottom.png")`;
+    bottomEl.style.backgroundImage = `url("./assets/${botValue}-pants.png")`;
     // update the stats to show the new count (call displayStats() to do this work)
     displayStats();
 });
 
 catchphraseButton.addEventListener('click', () => {
     // get the value of the catchphrase input
-    let catchphrase = catchphraseInput;
+    let newCatchphrase = catchphraseInput.value;
     // push the new catchphrase to the catchphrase array in state
-    catchphrases.push(catchphrase);
+    catchphrases.push(newCatchphrase);
     // clear out the form input's value so it's empty to the user
-    
+    catchphraseInput.value = '';
     // update the dom to show the new catchphrases (refactor to/call displayCatchphrases to do this work)
-
+    displayCatchphrases();
 });
 
 function displayStats() {
     // text content of the reportEl to tell the user how many times they've changed each piece of the state
+    reportEl.textContent = `You have changed the head ${headCount} times, the torso ${middleCount} times, and the legs the head ${bottomCount} times.`;
 }
 
 function displayCatchphrases() {
     // clear out the DOM for the currently displayed catchphrases
-
+    catchphrasesEl.textContent = '';
     // loop through each catchphrase in state
-    const catchphraseList = document.getElementById('catchphrase-list');
+    const catchphraseList = document.getElementById('catchphrases');
     // clear out the existing catchphrases
     
     for (let phrase of catchphrases) {
@@ -80,8 +82,8 @@ function displayCatchphrases() {
         // append the new element to the page
         catchphraseList.append(li);
     }
-    // and for each catchphrase
-    
+    // and for each catchphrase    
+
     // create an HTML element with the catchphrase as its text content
     
     // and append that HTML element to the cleared-out DOM
